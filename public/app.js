752e283ca0ca764bbc2626473fd2ab764d5c643f
Steven Winston <swinston100@hotmail.com>
2017-07-09 13:34:44 +0300

    finished

--- public/app.js
+++ public/app.js
@@ -1,16 +1,37 @@
 var app = angular.module('scotcha', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/home');
   $stateProvider.state('home', {
       url: '/home',
-      templateUrl: 'partial-home.html'
+      templateUrl: './templates/partial-home.html'
     })
-    .state('away', {
-      url: '/away',
-      template: '<h2>We are Away</h2>'
-    })
-    .state('home.about', {
+    .state('home.list', {
+      url: '/list',
+      templateUrl: './templates/partial-home-list.html',
+      controller: function($scope) {
+        $scope.scotches = ['Balvenie', 'The Balmore', 'Highland Park'];
+      }
+    }).state('home.about', {
       url: '/about',
-      template: '<h2>We are awesome</h2>'
+      templateUrl: './templates/partial-home-about.html'
     });
+  $stateProvider.state('contact', {
+    url: '/contact',
+    templateUrl: './templates/partial-contact.html'
+  });
+  $stateProvider.state('scotch', {
+    url: '/scotch/:type?temp',
+    templateUrl: './templates/partial-scotch.html',
+    controller: function($scope, $stateParams) {
+      $scope.myScotch = $stateParams;
+    }
+  });
+  $stateProvider.state('scotchlibrary', {
+    url: '/library/:type',
+    templateUrl: function($stateParams) {
+      return './templates/partial-scotch-' + $stateParams.type + '.html';
+    }
+  });
+  $locationProvider.html5Mode(true);
 });
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+app.all('/templates/*', function(req, res) {
+  res.sendFile(__dirname + "/public/templates/errorTemplate.html")
+});
+
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+});
+
 app.listen(8000, function() {
   console.log("what do you want from me! get me on 8000 ;-)");
 });
